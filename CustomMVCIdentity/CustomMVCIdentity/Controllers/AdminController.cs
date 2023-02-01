02c0dcdba6fdd85d508fbbad0bbb58964e0583ac
khan- sheeba <khansheebaismail@gmail.com>
2023-02-01 17:32:14 +0530

    Login Authentication

---
+++ CustomMVCIdentity/CustomMVCIdentity/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IdentityManagement.DAL;
+using IdentityManagement.Data;
+using IdentityManagement.Entities;
+
+namespace CustomMVCIdentity.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetUser(string id)
+        {
+            ApplicationUser user = UserController.GetUser(id);
+            return View(user);
+        }
+
+        public ActionResult GetUserByUsername(string userName)
+        {
+            ApplicationUser user = UserController.GetUserByUsername(userName);
+            return View(user);
+        }
+    }
+}
