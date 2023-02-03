065cb7bac85515ea4587f2a31bf3260c74d79503
khan- sheeba <khansheebaismail@gmail.com>
2023-02-03 17:20:39 +0530

    Login LogOut Crud with identity

---
+++ LoginAuth/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using LoginAuth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginAuth.Data
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options) { }
+    public DbSet<UserEntity> userEntities { get; set; }
+    }
+}
---
+++ LoginAuth/Migrations/20230202173430_IniatialDatabaseSetup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoginAuth.Migrations
+{
+    /// <inheritdoc />
+    public partial class IniatialDatabaseSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "userEntities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mobile = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_userEntities", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "userEntities");
+        }
+    }
+}
